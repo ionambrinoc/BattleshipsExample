e90c231c8d760312a62e0e24647d67675675416e
Jiang Yingxin <jiang.yingxin@softwire.com>
2014-06-16 15:04:14 +0200

    Add T4MVC

--- Battleships.Web/Controllers/HomeController.cs
+++ Battleships.Web/Controllers/HomeController.cs
@@ -2,9 +2,9 @@
 {
     using System.Web.Mvc;
 
-    public class HomeController : Controller
+    public partial class HomeController : Controller
     {
-        public ActionResult Index()
+        public virtual ActionResult Index()
         {
             return View();
         }
