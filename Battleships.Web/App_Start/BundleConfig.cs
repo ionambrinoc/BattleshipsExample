f4d8a89bb9aedee6983ced2d01c7354e96614797
Jiang Yingxin <jiang.yingxin@softwire.com>
2014-06-16 16:51:29 +0200

    Configure the bundles

--- Battleships.Web/App_Start/BundleConfig.cs
+++ Battleships.Web/App_Start/BundleConfig.cs
@@ -1,5 +1,6 @@
 ﻿namespace Battleships.Web
 {
+    using BundleTransformer.Core.Bundles;
     using System.Web.Optimization;
 
     public class BundleConfig
@@ -7,28 +8,16 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new CustomScriptBundle("~/bundles/scripts/common").Include(
+                "~/Components/jquery/dist/jquery.js",
+                "~/Components/jquery.validation/dist/jquery.validate.js",
+                "~/Components/Microsoft.jQuery.Unobtrusive.Validation/jquery.validate.unobtrusive.js",
+                "~/Components/bootstrap/dist/js/bootstrap.js",
+                "~/Components/respond/dest/respond.src.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js",
-                "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
-
-            // Set EnableOptimizations to false for debugging. For more information,
-            // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = true;
+            bundles.Add(new CustomStyleBundle("~/bundles/styles/common").Include(
+                "~/Components/bootstrap/less/bootstrap.less",
+                "~/Content/site.less"));
         }
     }
 }
