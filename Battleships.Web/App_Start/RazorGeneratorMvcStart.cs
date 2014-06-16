2a21eca4f76a1411f1415d460d0422b5c763ce45
Jiang Yingxin <jiang.yingxin@softwire.com>
2014-06-16 15:53:56 +0200

    Tidy up RazorGenerator

--- Battleships.Web/App_Start/RazorGeneratorMvcStart.cs
+++ Battleships.Web/App_Start/RazorGeneratorMvcStart.cs
@@ -1,20 +1,27 @@
-using System.Web;
-using System.Web.Mvc;
-using System.Web.WebPages;
-using RazorGenerator.Mvc;
+using Battleships.Web.App_Start;
+using WebActivatorEx;
 
-[assembly: WebActivatorEx.PostApplicationStartMethod(typeof(Battleships.Web.App_Start.RazorGeneratorMvcStart), "Start")]
+[assembly: PostApplicationStartMethod(typeof(RazorGeneratorMvcStart), "Start")]
 
-namespace Battleships.Web.App_Start {
-    public static class RazorGeneratorMvcStart {
-        public static void Start() {
-            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly) {
-                UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
-            };
+namespace Battleships.Web.App_Start
+{
+    using RazorGenerator.Mvc;
+    using System.Web;
+    using System.Web.Mvc;
+    using System.Web.WebPages;
+
+    public static class RazorGeneratorMvcStart
+    {
+        public static void Start()
+        {
+            var engine = new PrecompiledMvcEngine(typeof(RazorGeneratorMvcStart).Assembly)
+                         {
+                             UsePhysicalViewsIfNewer = HttpContext.Current.Request.IsLocal
+                         };
 
             ViewEngines.Engines.Insert(0, engine);
 
-            // StartPage lookups are done by WebPages. 
+            // StartPage lookups are done by WebPages.
             VirtualPathFactoryManager.RegisterVirtualPathFactory(engine);
         }
     }
