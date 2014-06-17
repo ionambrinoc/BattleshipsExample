5f61517857bd9543af5f031076ec6e4e92f40d79
Jiang Yingxin <jiang.yingxin@softwire.com>
2014-06-17 11:19:17 +0200

    Add Battleships Runner and Test projects.

---
+++ Battleships.Runner.Tests/HeadToHeadTests.cs
@@ -0,0 +1,7 @@
+﻿namespace Battleships.Runner.Tests
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class HeadToHeadTests {}
+}
---
+++ Battleships.Runner/HeadToHead.cs
@@ -0,0 +1,23 @@
+﻿namespace Battleships.Runner
+{
+    using Battleships.Player;
+    using System;
+
+    public class HeadToHead
+    {
+        private readonly IBattleshipsPlayer player1;
+        private readonly IBattleshipsPlayer player2;
+
+        public HeadToHead(IBattleshipsPlayer player1, IBattleshipsPlayer player2)
+        {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        public IBattleshipsPlayer RunGame()
+        {
+            var random = new Random();
+            return random.Next(2) == 0 ? player1 : player2;
+        }
+    }
+}
