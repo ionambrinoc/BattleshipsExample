1582cdf8ea052b94c38630a58b26ea01e33ae56b
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-06-26 17:50:25 +0100

    Add DbContext and kittens

---
+++ Battleships.Runner/BattleshipsContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Battleships.Runner
+{
+    using Battleships.Runner.Models;
+    using System.Data.Entity;
+
+    public class BattleshipsContext : DbContext
+    {
+        public BattleshipsContext() : base("DefaultConnection") {}
+
+        public DbSet<Kitten> Kittens { get; set; }
+    }
+}
---
+++ Battleships.Runner/Models/Kitten.cs
@@ -0,0 +1,8 @@
+﻿namespace Battleships.Runner.Models
+{
+    public class Kitten
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
