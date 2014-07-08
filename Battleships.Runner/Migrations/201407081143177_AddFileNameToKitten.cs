a5a99fd3252c16aeb464fce5161ff7bf65b02cd6
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-08 13:33:16 +0100

    Add migration for kitten file name

---
+++ Battleships.Runner/Migrations/201407081143177_AddFileNameToKitten.cs
@@ -0,0 +1,17 @@
+namespace Battleships.Runner.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class AddFileNameToKitten : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Kittens", "FileName", c => c.String());
+        }
+
+        public override void Down()
+        {
+            DropColumn("dbo.Kittens", "FileName");
+        }
+    }
+}
