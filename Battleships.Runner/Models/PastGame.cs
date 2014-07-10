9a0ac7901bc17e7adc845956ec4a3a61b01a7904
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-10 18:53:27 +0100

    Fix static analysis warnings

--- Battleships.Runner/Models/PastGame.cs
+++ Battleships.Runner/Models/PastGame.cs
@@ -8,29 +8,11 @@
         public string SecondPlayer { get; set; }
         public bool FirstPlayerWon { get; set; }
         public DateTime TimePlayed { get; set; }
-        public string Winner {
-            get
-            {
-                if (FirstPlayerWon)
-                {
-                    return FirstPlayer;
-                }
-                else
-                {
-                    return SecondPlayer;
-                }
-            }
-            set
-            {
-                if (value == FirstPlayer)
-                {
-                    FirstPlayerWon = true;
-                }
-                else
-                {
-                    FirstPlayerWon = false;
-                }
-            }
+
+        public string Winner
+        {
+            get { return FirstPlayerWon ? FirstPlayer : SecondPlayer; }
+            set { FirstPlayerWon = value == FirstPlayer; }
         }
     }
 }
