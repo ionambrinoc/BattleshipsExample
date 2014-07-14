4bb8db25582d2e6ddb9b4a055dc46df1840ac8e5
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-14 22:01:33 +0100

    Styling for Players (head-to-head) page

--- Battleships.Web/Scripts/Players/index.js
+++ Battleships.Web/Scripts/Players/index.js
@@ -2,56 +2,62 @@
 window.battleships.players = window.battleships.players || {};
 
 window.battleships.players.index = (function($, undefined) {
+    var firstPlayer = $('#firstPlayer');
+    var secondPlayer = $('#secondPlayer');
 
-    var firstPlayerId;
-    var secondPlayerId;
 
     function addToGame(id, name) {
-        if (!firstPlayerId) {
-            firstPlayerId = id;
-            $("#firstPlayer").text(name);
-        } else if (!secondPlayerId) {
-            secondPlayerId = id;
-            $("#secondPlayer").text(name);
+        if (!firstPlayer.data("id")) {
+            firstPlayer.data("id", id);
+            addPlayerNameToForm(firstPlayer, name);
+        } else if (!secondPlayer.data("id")) {
+            secondPlayer.data("id", id);
+            addPlayerNameToForm(secondPlayer, name);
         } else {
             alert("There are already two players added to the game.");
         }
     }
 
-    function resetFirstPlayer() {
-        $("#firstPlayer").text("First player");
-        firstPlayerId = null;
+    function addPlayerNameToForm(element, name) {
+        element.text(name);
+        element.addClass("filled");
+        element.addClass("x");
     }
 
-    function resetSecondPlayer() {
-        $("#secondPlayer").text("Second player");
-        secondPlayerId = null;
+    function resetPlayer(element) {
+        element = $(element);
+        element.text(element.data("default-text"));
+        element.data("id", null);
+        element.removeClass("filled x onX");
     }
 
     function setUpButton() {
         addToGame($(this).data('player-id'), $(this).data('player-name'));
     }
+    function tog(v) { return v ? 'addClass' : 'removeClass'; }
 
     return {
         init: function() {
-
-            $("#firstPlayerReset").click(resetFirstPlayer);
-            $("#secondPlayerReset").click(resetSecondPlayer);
-
-            $('#playGame').click(function() {
-                if (firstPlayerId && secondPlayerId) {
-                    $('#playerOneId').val(firstPlayerId);
-                    $('#playerTwoId').val(secondPlayerId);
+            $('#runGameButton').click(function() {
+                if (firstPlayer.data('id') && secondPlayer.data('id')) {
+                    $('#playerOneId').val(firstPlayer.data('id'));
+                    $('#playerTwoId').val(secondPlayer.data('id'));
 
                     $('#run-game-form').ajaxSubmit(function(data) {
-                        $("#winner").text(data);
+                        $("#winner").text(data + " wins!");
                     });
                 } else {
                     alert("You need two players to start the game.");
                 }
             });
 
-            $('#playerTable button').each(function() { $(this).on('click', setUpButton); });
+            $('#playerTable button').each(function () { $(this).on('click', setUpButton); });
+
+            $(document).on('mousemove', '.x', function (e) {
+                $(this)[tog(this.offsetWidth - 100 < e.clientX - this.getBoundingClientRect().left)]('onX');
+            }).on('click', '.onX', function () {
+                resetPlayer(this);
+            });
         }
     };
 })(jQuery);
