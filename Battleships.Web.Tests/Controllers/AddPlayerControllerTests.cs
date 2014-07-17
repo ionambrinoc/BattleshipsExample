d4aa48c89417fecd8fd68449f6e91d2da296aa54
Tomas Vaskevicius <Tomas.Vaskevicius@softwire.com>
2014-07-17 14:10:06 +0100

    Added tests for AddPlayerController

--- Battleships.Web.Tests/Controllers/AddPlayerControllerTests.cs
+++ Battleships.Web.Tests/Controllers/AddPlayerControllerTests.cs
@@ -1,96 +1,173 @@
 ﻿namespace Battleships.Web.Tests.Controllers
 {
+    using Battleships.Player;
     using Battleships.Runner.Models;
     using Battleships.Runner.Repositories;
     using Battleships.Runner.Tests.TestHelpers;
     using Battleships.Web.Controllers;
+    using Battleships.Web.Models.AddPlayer;
     using Battleships.Web.Services;
     using Battleships.Web.Tests.TestHelpers.NUnitConstraints;
     using FakeItEasy;
+    using FluentAssertions;
     using NUnit.Framework;
+    using System;
     using System.Configuration;
+    using System.IO;
     using System.Web;
     using System.Web.Mvc;
     using System.Web.Routing;
 
     internal class AddPlayerControllerTests
     {
-        private const string TestPlayerUserName = "My Test User";
-        private const string TestPlayerName = "My Test Player";
-        private const string UserNameField = "userName";
-        private const string NameField = "name";
-        private PlayerRecord playerRecordOne;
-        private PlayerRecord playerRecordTwo;
-        private FormCollection formInput;
+        private const string UserName = "testUser";
         private AddPlayerController controller;
         private IPlayerRecordsRepository fakePlayerRecordRepository;
         private IPlayerUploadService fakePlayerUploadService;
         private HttpPostedFileBase fakeFile;
+        private IBattleshipsPlayer fakeBattleshipsPlayer;
+        private PlayerRecord fakePlayerRecord;
 
         [SetUp]
         public void SetUp()
         {
             ConfigurationManager.AppSettings["PlayerStoreDirectory"] = TestPlayerStore.Directory;
-
-            formInput = new FormCollection { { UserNameField, TestPlayerUserName }, { NameField, TestPlayerName } };
             fakePlayerRecordRepository = A.Fake<IPlayerRecordsRepository>();
             fakePlayerUploadService = A.Fake<IPlayerUploadService>();
             controller = new AddPlayerController(fakePlayerRecordRepository, fakePlayerUploadService) { ControllerContext = GetFakeControllerContext() };
-            playerRecordOne = A.Fake<PlayerRecord>();
-            playerRecordTwo = A.Fake<PlayerRecord>();
-            playerRecordOne.Id = 1;
-            playerRecordTwo.Id = 2;
-            playerRecordOne.Name = "Kitten";
-            playerRecordTwo.Name = "KittenTwo";
+            fakeBattleshipsPlayer = A.Fake<IBattleshipsPlayer>();
+            fakeFile = A.Fake<HttpPostedFileBase>();
+            fakePlayerRecord = A.Fake<PlayerRecord>();
+            A.CallTo(() => controller.User.Identity.Name).Returns(UserName);
+            A.CallTo(() => fakePlayerUploadService.GetBattleshipsPlayerFromHttpPostedFileBase(A<HttpPostedFileBase>.Ignored)).Returns(fakeBattleshipsPlayer);
+            A.CallTo(() => fakeBattleshipsPlayer.Name).Returns("testName");
         }
 
         [Test]
-        public void Index_redirects_to_loginview_if_not_authorised()
+        public void Index_GET_redirects_to_login_view_when_not_authorised()
         {
-            //TODO: fix fake user authorization
+            // Given
+            A.CallTo(() => controller.Request.IsAuthenticated).Returns(false);
 
             // When
-            var view = controller.Index();
+            var result = controller.Index();
 
             // Then
-            Assert.That(view, IsMVC.RedirectTo(MVC.Account.LogIn()));
+            Assert.That(result, IsMVC.RedirectTo(MVC.Account.LogIn()));
         }
 
         [Test]
-        public void Posting_to_index_redirects_to_index()
+        public void Index_GET_redirects_to_index_view_when_authorised()
         {
+            // Given
+            A.CallTo(() => controller.Request.IsAuthenticated).Returns(true);
+
             // When
-            var result = controller.Index(formInput);
+            var result = controller.Index();
+
+            // Then
+            Assert.That(result, IsMVC.View(""));
+            Assert.IsFalse(controller.ViewData.Model.As<AddPlayerModel>().CanOverwrite);
+        }
+
+        [Test]
+        public void Index_POST_redirects_to_view_when_model_has_an_error()
+        {
+            // Given
+            var model = new AddPlayerModel();
+            controller.ModelState.AddModelError("test", "testError");
+
+            // When
+            var result = controller.Index(model);
+
+            // Then
+            Assert.That(result, IsMVC.View(""));
+        }
+
+        [Test]
+        public void Index_POST_uploading_a_bot_with_botname_this_user_already_has_updates_model_state_and_returns_view()
+        {
+            // Given
+            var model = new AddPlayerModel { CanOverwrite = false, File = fakeFile };
+            A.CallTo(() => fakePlayerRecordRepository.UserWithGivenBotName(fakeBattleshipsPlayer.Name)).Returns(UserName);
+
+            // When
+            var result = controller.Index(model);
+
+            // Then
+            Assert.That(result, IsMVC.View(""));
+            Assert.IsTrue(controller.ViewData.Model.As<AddPlayerModel>().CanOverwrite);
+        }
+
+        [Test]
+        public void Index_POST_does_not_allow_to_overwrite_other_users_bot()
+        {
+            // Given
+            var model = new AddPlayerModel { CanOverwrite = false, File = fakeFile };
+            A.CallTo(() => fakePlayerRecordRepository.UserWithGivenBotName(fakeBattleshipsPlayer.Name)).Returns("otherUser");
+
+            // When
+            var result = controller.Index(model);
+
+            // Then
+            Assert.That(result, IsMVC.View(""));
+            Assert.That(controller, HasMVC.ModelLevelErrors());
+        }
+
+        [Test]
+        public void Index_POST_posting_file_with_new_bot_name_fails_if_bot_file_is_not_valid()
+        {
+            // Given
+            var model = new AddPlayerModel { CanOverwrite = false, File = fakeFile };
+            A.CallTo(() => fakePlayerUploadService.UploadAndGetPlayerRecord("testUser", fakeFile, A<string>.Ignored)).Throws(new Exception());
+            A.CallTo(() => fakePlayerRecordRepository.UserWithGivenBotName(fakeBattleshipsPlayer.Name)).Returns("");
+
+            // When
+            var result = controller.Index(model);
+
+            // Then
+            Assert.That(result, IsMVC.View(""));
+            Assert.That(controller, HasMVC.ModelLevelErrors());
+        }
+
+        [Test]
+        public void Index_POST_posting_file_with_new_bot_name_succeeds_if_bot_file_is_valid()
+        {
+            // Given
+            var model = new AddPlayerModel { CanOverwrite = false, File = fakeFile };
+            A.CallTo(() => fakePlayerRecordRepository.UserWithGivenBotName(fakeBattleshipsPlayer.Name)).Returns("");
+            A.CallTo(() => fakePlayerUploadService.UploadAndGetPlayerRecord("testUser", fakeFile, A<string>.Ignored)).Returns(fakePlayerRecord);
+
+            // When
+            var result = controller.Index(model);
+
+            // Then
+            A.CallTo(() => fakePlayerRecordRepository.Add(fakePlayerRecord)).MustHaveHappened();
+            A.CallTo(() => fakePlayerRecordRepository.SaveContext()).MustHaveHappened();
+            Assert.That(result, IsMVC.RedirectTo(MVC.Players.Index()));
+        }
+
+        [Test]
+        public void OverwriteYes_redirects_to_players_index()
+        {
+            // Given
+            var model = new AddPlayerModel { TemporaryPath = Path.GetTempFileName(), RealPath = Path.GetTempFileName() };
+
+            // When
+            var result = controller.OverwriteYes(model);
 
             // Then
             Assert.That(result, IsMVC.RedirectTo(MVC.Players.Index()));
         }
 
         [Test]
-        public void Posting_code_file_delegates_to_player_upload_service()
+        public void OverwriteNo_redirects_to_addplayer_index()
         {
             // When
-            controller.Index(formInput);
+            var result = controller.OverwriteNo();
 
             // Then
-            A.CallTo(() => fakePlayerUploadService.UploadAndGetPlayerRecord("", fakeFile, TestPlayerStore.Directory))
-             .MustHaveHappened();
-        }
-
-        [Test]
-        public void Posting_code_file_saves_new_player()
-        {
-            // Given
-            var fakePlayer = A.Fake<PlayerRecord>();
-            A.CallTo(() => fakePlayerUploadService.UploadAndGetPlayerRecord(A<string>._, A<HttpPostedFileBase>._, A<string>._))
-             .Returns(fakePlayer);
-
-            // When
-            controller.Index(formInput);
-
-            // Then
-            A.CallTo(() => fakePlayerRecordRepository.Add(fakePlayer)).MustHaveHappened();
-            A.CallTo(() => fakePlayerRecordRepository.SaveContext()).MustHaveHappened();
+            Assert.That(result, IsMVC.RedirectTo(MVC.AddPlayer.Index()));
         }
 
         private ControllerContext GetFakeControllerContext()
