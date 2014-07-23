7b277d6a8cd4b6f4c0aece3376664e290d2976bd
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-23 19:08:27 +0100

    Fix static analysis warnings

--- Battleships.Runner/Migrations/201407231457533_RenamePictureNameToPictureFileName.cs
+++ Battleships.Runner/Migrations/201407231457533_RenamePictureNameToPictureFileName.cs
@@ -1,8 +1,7 @@
 namespace Battleships.Runner.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class RenamePictureNameToPictureFileName : DbMigration
     {
         public override void Up()
@@ -10,7 +9,7 @@ namespace Battleships.Runner.Migrations
             AddColumn("dbo.PlayerRecords", "PictureFileName", c => c.String());
             DropColumn("dbo.PlayerRecords", "PictureName");
         }
-        
+
         public override void Down()
         {
             AddColumn("dbo.PlayerRecords", "PictureName", c => c.String());
