491e126510ab20d45a34b1c0e4442fdedbd631ae
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-23 10:33:57 +0100

    Fix Users/PlayerRecords FK migration to allow existing data.

--- Battleships.Runner/Migrations/201407221259100_AddedForeignKeyToUsersTableInPlayerRecords.cs
+++ Battleships.Runner/Migrations/201407221259100_AddedForeignKeyToUsersTableInPlayerRecords.cs
@@ -6,7 +6,17 @@ namespace Battleships.Runner.Migrations
     {
         public override void Up()
         {
-            AddColumn("dbo.PlayerRecords", "UserId", c => c.String(false, 128));
+            AddColumn("dbo.PlayerRecords", "UserId", c => c.String(true, 128));
+
+            Sql(@"
+UPDATE dbo.PlayerRecords
+SET UserId = U.Id
+FROM dbo.PlayerRecords P
+JOIN dbo.AspNetUsers U
+ON P.UserName = U.UserName");
+
+            AlterColumn("dbo.PlayerRecords", "UserId", c => c.String(false, 128));
+
             CreateIndex("dbo.PlayerRecords", "UserId");
             AddForeignKey("dbo.PlayerRecords", "UserId", "dbo.AspNetUsers", "Id");
             DropColumn("dbo.PlayerRecords", "UserName");
@@ -15,6 +25,14 @@ namespace Battleships.Runner.Migrations
         public override void Down()
         {
             AddColumn("dbo.PlayerRecords", "UserName", c => c.String());
+
+            Sql(@"
+UPDATE dbo.PlayerRecords
+SET UserName = U.UserName
+FROM dbo.PlayerRecords P
+JOIN dbo.AspNetUsers U
+ON P.UserId = U.Id");
+
             DropForeignKey("dbo.PlayerRecords", "UserId", "dbo.AspNetUsers");
             DropIndex("dbo.PlayerRecords", new[] { "UserId" });
             DropColumn("dbo.PlayerRecords", "UserId");
