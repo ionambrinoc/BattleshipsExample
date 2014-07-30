f9f24c49c8276deb13b4aa2ca33b102c8697bee9
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-07-30 10:53:23 +0100

    Add database test helpers

---
+++ Battleships.Core.Tests/TestHelpers/Database/TestBattleshipsContext.cs
@@ -0,0 +1,14 @@
+﻿namespace Battleships.Core.Tests.TestHelpers.Database
+{
+    using Battleships.Core;
+    using System.Data.Entity;
+
+    public class TestBattleshipsContext : BattleshipsContext
+    {
+        public TestBattleshipsContext() {}
+
+        public TestBattleshipsContext(string nameOrConnectionString) : base(nameOrConnectionString) {}
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+    }
+}
---
+++ Battleships.Core.Tests/TestHelpers/Database/TestDb.cs
@@ -0,0 +1,31 @@
+﻿namespace Battleships.Core.Tests.TestHelpers.Database
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class TestDb
+    {
+        public static string ConnectionString
+        {
+            get { return String.Format("Data Source={0}.sdf", TestContext.CurrentContext.Test.FullName); }
+        }
+
+        public static List<T> GetAll<T>() where T : class
+        {
+            using (var context = new TestBattleshipsContext(ConnectionString))
+            {
+                return context.Set<T>().ToList();
+            }
+        }
+
+        public static void Delete()
+        {
+            using (var context = new TestBattleshipsContext(ConnectionString))
+            {
+                context.Database.Delete();
+            }
+        }
+    }
+}
