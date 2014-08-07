1c0f8bd78ff2e22ee81fcd6bb19ea2c630cd9cbd
Jiang Yingxin <Jiang.Yingxin@softwire.com>
2014-08-07 18:27:07 +0100

    Add tests for FilterConfig

---
+++ Battleships.Web.Tests/FilterConfigTests.cs
@@ -0,0 +1,30 @@
+﻿namespace Battleships.Web.Tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Linq;
+    using System.Web.Mvc;
+
+    [TestFixture]
+    public class FilterConfigTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            GlobalFilters.Filters.Clear();
+        }
+
+        [TestCase(typeof(HandleErrorAttribute))]
+        public void Type_is_registered_as_filter(Type filterType)
+        {
+            var filters = GlobalFilters.Filters.Select(f => f.Instance);
+            Assert.That(filters, Has.Exactly(1).TypeOf(filterType));
+        }
+    }
+}
