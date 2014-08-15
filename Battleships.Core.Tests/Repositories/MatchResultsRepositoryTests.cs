a356b2f4a7d463614b437ddcbeda4d2ef123dffb
Kenneth Siu <Kenneth.Siu@softwire.com>
2014-08-15 10:10:32 +0100

    Cleaned up unit tests for readability.

--- Battleships.Core.Tests/Repositories/MatchResultsRepositoryTests.cs
+++ Battleships.Core.Tests/Repositories/MatchResultsRepositoryTests.cs
@@ -15,16 +15,14 @@
     [RequiresDatabase]
     public class MatchResultsRepositoryTests
     {
-        private readonly DateTime date1 = new DateTime(1955, 8, 26, 1, 20, 0);
-        private readonly DateTime date2 = new DateTime(1985, 8, 26, 1, 22, 0);
-        private readonly DateTime date3 = new DateTime(2012, 6, 27, 1, 21, 0);
+        private readonly DateTime date1 = new DateTime(2001, 1, 1);
+        private readonly DateTime date2 = new DateTime(2002, 1, 1);
         private MatchResultsRepository repo;
         private PlayerRecord player1;
         private PlayerRecord player2;
         private PlayerRecord player3;
         private MatchResult matchResult1;
         private MatchResult matchResult2;
-        private MatchResult matchResult3;
         private string userId;
 
         [SetUp]
@@ -37,11 +35,9 @@
             player2 = CreatePlayer("Player2");
             player3 = CreatePlayer("Player3");
             matchResult1 = CreateMatchResult(player1, player2, date1);
-            matchResult2 = CreateMatchResult(player2, player3, date2);
-            matchResult3 = CreateMatchResult(player3, player1, date3);
+            matchResult2 = CreateMatchResult(player1, player3, date2);
             repo.Add(matchResult1);
             repo.Add(matchResult2);
-            repo.Add(matchResult3);
             repo.SaveContext();
         }
 
@@ -52,7 +48,7 @@
             var result = repo.GetMostRecentMatchTime();
 
             // Then
-            result.Should().Be(date3);
+            result.Should().Be(date2);
         }
 
         [Test]
@@ -60,7 +56,23 @@
         {
             // Given
             var newMatch = matchResult1;
-            newMatch.TimePlayed = date3;
+            newMatch.TimePlayed = date2;
+
+            // When
+            repo.UpdateResults(new List<MatchResult> { newMatch });
+            repo.SaveContext();
+
+            // Then
+            repo.GetAll().Count().Should().Be(2);
+            repo.GetAll().Count(match => match.TimePlayed == date2 && match.Winner == player1 && match.Loser == player2).Should().Be(1);
+            repo.GetAll().Count(match => match == newMatch).Should().Be(1);
+        }
+
+        [Test]
+        public void Will_add_result_if_players_have_not_played_each_other()
+        {
+            // Given
+            var newMatch = CreateMatchResult(player2, player3, DateTime.Now);
 
             // When
             repo.UpdateResults(new List<MatchResult> { newMatch });
@@ -68,22 +80,7 @@
 
             // Then
             repo.GetAll().Count().Should().Be(3);
-            repo.GetAll().Where(match => match.TimePlayed == date3 && match.Loser == player1 && match.Winner == player2).ToList().Count.Should().Be(1);
-            repo.GetAll().Where(match => match == newMatch).ToList().Count.Should().Be(1);
-        }
-
-        [Test]
-        public void Will_add_result_if_players_have_not_played_each_other()
-        {
-            // Given
-            var tempPlayer = CreatePlayer("playerTemp");
-            var newMatch = CreateMatchResult(player1, tempPlayer, DateTime.Now);
-
-            // When
-            repo.UpdateResults(new List<MatchResult> { newMatch });
-
-            // Then
-            repo.GetAll().Count().Should().Be(4);
+            repo.GetAll().Count(match => match.Winner == player2 && match.Loser == player3).Should().Be(1);
         }
 
         private PlayerRecord CreatePlayer(string name)
@@ -91,9 +88,9 @@
             return new PlayerRecord { Name = name, UserId = userId, LastUpdated = DateTime.Now };
         }
 
-        private MatchResult CreateMatchResult(PlayerRecord loser, PlayerRecord winner, DateTime timePlayed)
+        private MatchResult CreateMatchResult(PlayerRecord winner, PlayerRecord loser, DateTime timePlayed)
         {
-            return new MatchResult { Loser = loser, Winner = winner, TimePlayed = timePlayed };
+            return new MatchResult { Winner = winner, Loser = loser, TimePlayed = timePlayed };
         }
     }
 }
