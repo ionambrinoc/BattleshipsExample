cdaa82a848b7c2a8190728ffd37375291759dfaf
Matt Ely <matthew.ely@softwire.com>
2014-08-19 18:05:59 +0100

    Refactored circular dependencies and core models GameEvent and GameLog.

--- Battleships.Runner/Factories/GameLogFactory.cs
+++ Battleships.Runner/Factories/GameLogFactory.cs
@@ -1,34 +1,58 @@
-﻿namespace Battleships.Core.Factories
+﻿namespace Battleships.Runner.Factories
 {
     using Battleships.Core.Models;
+    using Battleships.Player;
     using Battleships.Player.Interface;
-    using Battleships.Runner.Models;
     using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.UI.WebControls.WebParts;
 
     public interface IGameLogFactory
     {
-        void InitialiseGameLog(PlayerRecord player1, PlayerRecord player2, DateTime startTime, IShipPosition[] player1Positions, IShipPosition[] player2Positions);
+        void InitialiseGameLog(PlayerRecord player1, PlayerRecord player2, DateTime startTime, IEnumerable<IShipPosition> player1Positions, IEnumerable<IShipPosition> player2Positions);
         void AddGameEvent(DateTime time, bool isPlayer1Turn, IGridSquare selectedTarget, bool isHit);
-        IGameLog GetCompleteGame(bool player1Won, ResultType resultType);
+        GameLog GetCompleteGame(bool player1Won, ResultType resultType);
     }
 
     public class GameLogFactory : IGameLogFactory
     {
+        private readonly GridSquareStringConverter converter;
         private GameLog gameLog;
-
-        public void InitialiseGameLog(PlayerRecord player1, PlayerRecord player2, DateTime startTime, IShipPosition[] player1Positions, IShipPosition[] player2Positions)
+        public GameLogFactory(GridSquareStringConverter converter)
         {
-            gameLog = new GameLog(player1, player2, startTime, player1Positions, player2Positions);
+            this.converter = converter;
+        }
+
+        public void InitialiseGameLog(PlayerRecord player1, PlayerRecord player2, DateTime startTime, IEnumerable<IShipPosition> player1Positions, IEnumerable<IShipPosition> player2Positions)
+        {
+            gameLog = new GameLog
+                      {
+                          Player1 = player1,
+                          Player2 = player2,
+                          StartTime = startTime,
+                          Player1PositionsString = player1Positions.Aggregate("", (s, position) => s + converter.ShipPositionToString(position)),
+                          Player2PositionsString = player2Positions.Aggregate("", (s, position) => s + converter.ShipPositionToString(position))
+                      };
         }
 
         public void AddGameEvent(DateTime time, bool isPlayer1Turn, IGridSquare selectedTarget, bool isHit)
         {
-            gameLog.AddGameEvent(time, isPlayer1Turn, selectedTarget, isHit);
+            var selectedTargetString = converter.GridSquareToString(selectedTarget);
+            gameLog.DetailedLog.Add(new GameEvent
+                                    {
+                                        IsHit = isHit,
+                                        IsPlayer1Turn = isPlayer1Turn,
+                                        SelectedTarget = selectedTargetString,
+                                        Time = time
+                                    });
         }
 
-        public IGameLog GetCompleteGame(bool player1Won, ResultType resultType)
+        public GameLog GetCompleteGame(bool player1Won, ResultType resultType)
         {
-            gameLog.SetGameResult(player1Won, resultType);
+            gameLog.Player1Won = player1Won;
+            gameLog.ResultType = resultType;
             return gameLog;
         }
     }
---
+++ Battleships.Runner/Helper/GridSquareStringConverter.cs
@@ -0,0 +1,18 @@
+﻿namespace Battleships.Runner.Factories
+{
+    using Battleships.Player.Interface;
+
+    public class GridSquareStringConverter 
+    {
+        public string ShipPositionToString(IShipPosition shipPosition)
+        {
+            return GridSquareToString(shipPosition.StartingSquare) + GridSquareToString(shipPosition.EndingSquare);
+        }
+
+        public string GridSquareToString(IGridSquare gridSquare)
+        {
+            return gridSquare.Row + gridSquare.Column.ToString();
+        }
+
+    }
+}
